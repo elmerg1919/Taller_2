b615ba13a2293ba1f8b5b3b09e569ea273368f28
elmerg1919 <61604682+elmerg1919@users.noreply.github.com>
2021-10-11 18:26:48 -0600

    cambio

--- Ejercicio 2/JS/Funciones.js
+++ Ejercicio 2/JS/Funciones.js
@@ -96,7 +96,7 @@ document.addEventListener('keydown', function(event) {
 document.addEventListener('keydown', function(event) {
   if (event.ctrlKey && event.key === 's') {
     event.preventDefault()
-    alert("Comentario guardado exitosamente :D")
+    alert("Su comentario obtenido con exito")
    
     }
 });
